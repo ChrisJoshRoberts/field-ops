5123c04d99377244d5e2ad262d4c1bc086c97bb0
ChrisJoshRoberts <chrisrobertscollective@gmail.com>
2026-01-08 12:28:14 +0200

    Init SupaBase in Main()

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:field_ops/features/auth/screens/login_screen.dart';
 import 'package:field_ops/features/dashboard/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -8,10 +9,13 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: ".env");
+
   await Supabase.initialize(
-    url: 'YOUR_SUPABASE_URL',
-    anonKey: 'YOUR_SUPABASE_ANON_KEY',
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
+
 
   runApp(const ProviderScope(child: MyApp()));
 }
